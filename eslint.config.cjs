da07750888bc8afe693e1e73b99037aeb04e6481
Andre <andre@tos.world>
2025-02-20 07:10:54 -0300

    fix: eslint

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,6 +2,7 @@ const js = require("@eslint/js");
 const tseslint = require("@typescript-eslint/eslint-plugin");
 const tsparser = require("@typescript-eslint/parser");
 const prettier = require("eslint-plugin-prettier");
+const globals = require("globals");
 
 module.exports = [
     js.configs.recommended,
@@ -9,10 +10,14 @@ module.exports = [
         languageOptions: {
             parser: tsparser,
             parserOptions: {
+                ecmaVersion: 2015,
                 project: "./tsconfig.json",
                 tsconfigRootDir: __dirname,
                 sourceType: "module"
-            }
+            },
+            globals: {
+                ...globals.node,
+            },
         },
         files: ["src/**/*.ts", "apps/**/*.ts", "libs/**/*.ts", "test/**/*.ts"],
         ignores: ["node_modules", "dist", "build", ".generated", "eslint.config.js"],
@@ -27,8 +32,7 @@ module.exports = [
             "@typescript-eslint/explicit-function-return-type": "off",
             "@typescript-eslint/explicit-module-boundary-types": "off",
             "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-unused-vars": "off",
-            "@typescript-eslint/no-unsafe-function-type": "off"
+            "@typescript-eslint/no-unused-vars": "off"
         },
         settings: {
             env: {
